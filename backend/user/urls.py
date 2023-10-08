3f76858bd6ecdbd6355f8c378cad6fe081a58e12
Sungmin <sungsung718@gmail.com>
2023-10-08 18:54:37 +0900

    fix: remove User app in wrong directory

--- backend/user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from user.views import RegisterView, LoginView, UserRetrieveUpdateView
-
-urlpatterns = [
-    path('register', RegisterView.as_view()),
-    path('login', LoginView.as_view()),
-    path('info', UserRetrieveUpdateView.as_view())
-]
