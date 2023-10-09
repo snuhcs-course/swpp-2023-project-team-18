fa702cc2ada06e89fd7d5de8e17e6f81857ee1c9
impri2 <impri@naver.com>
2023-10-09 22:45:00 +0900

    fix: Reimplement emotion literals with Enum

--- backend/moment/writing/constants.py
+++ backend/moment/writing/constants.py
@@ -1,16 +1,19 @@
-EMOTIONS_CHOICES = [
-    ("excited1", "excited1"),
-    ("excited2", "excited2"),
-    ("happy1", "happy1"),
-    ("happy2", "happy2"),
-    ("normal1", "normal1"),
-    ("normal2", "normal2"),
-    ("sad1", "sad1"),
-    ("sad2", "sad2"),
-    ("angry1", "angry1"),
-    ("angry2", "angry2"),
-]
+from enum import Enum
+
 
 MOMENT_MAX_LENGTH = 1000
 STORY_MAX_LENGTH = 10000
 STORY_TITLE_MAX_LENGTH = 100
+
+
+class Emotions(str, Enum):
+    EXCITED1 = "excited1"
+    EXCITED2 = "excited2"
+    HAPPY1 = "happy1"
+    HAPPY2 = "happy2"
+    NORMAL1 = "normal1"
+    NORMAL2 = "normal2"
+    SAD1 = "sad1"
+    SAD2 = "sad2"
+    ANGRY1 = "angry1"
+    ANGRY2 = "angry2"
--- backend/moment/writing/models.py
+++ backend/moment/writing/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from user.models import User
 from .constants import (
-    EMOTIONS_CHOICES,
     MOMENT_MAX_LENGTH,
     STORY_MAX_LENGTH,
     STORY_TITLE_MAX_LENGTH,
+    Emotions,
 )
 
 # Create your models here.
@@ -19,7 +19,9 @@ class Story(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     emotion = models.CharField(
-        max_length=100, choices=EMOTIONS_CHOICES, default="normal1"
+        max_length=100,
+        choices=[(e.value, e.value) for e in Emotions],
+        default=Emotions.NORMAL1,
     )
     score = models.IntegerField(default=3)
     is_point_completed = models.BooleanField(default=False)
