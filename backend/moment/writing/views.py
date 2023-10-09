b8dafee06aa24e2a468aef07f174f3c8e76d9888
aylee2008 <aylee2008@naver.com>
2023-10-09 16:20:38 +0900

    fix: Change code to match user model

--- backend/moment/writing/views.py
+++ backend/moment/writing/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 
 from .models import MomentPair
 from .serializers import MomentPairQuerySerializer, MomentPairSerializer
+from user.models import User
 
 
 # Create your views here.
@@ -16,7 +17,7 @@ class MomentView(GenericAPIView):
     def get(self, request):
         params = MomentPairQuerySerializer(data=request.query_params)
         params.is_valid(raise_exception=True)
-        user = request.user
+        user=User.objects.get(pk=request.user.id)
 
         start_date = datetime.fromtimestamp(params.validated_data["start"])
         end_date = datetime.fromtimestamp(params.validated_data["end"])
