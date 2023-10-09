06e84a25e531efcfc77779efce6c71ac795d7433
박정헌 <impri@naver.om>
2023-10-09 13:04:57 +0900

    feat: implement timestamp conversion of moment

--- backend/moment/writing/serializers.py
+++ backend/moment/writing/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework.serializers import ModelSerializer
-from models import MomentPair
+from rest_framework.serializers import IntegerField
+from .models import MomentPair
 
 
 class MomentPairSerializer(ModelSerializer):
     class Meta:
         model = MomentPair
         fields = ["id", "moment", "reply", "moment_created_at", "reply_created_at"]
+
+    def to_representation(self, instance: MomentPair):
+        res = super().to_representation(instance)
+        res["moment_created_at"] = int(instance.moment_created_at.timestamp())
+        res["reply_created_at"] = int(instance.reply_created_at.timestamp())
+        return res
