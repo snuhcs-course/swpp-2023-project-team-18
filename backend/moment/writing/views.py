8960d3432c52b793d9c7ba1d67476dff6346a0f1
footprinthere <tai15515@kakao.com>
2023-10-09 13:56:22 +0900

    feature: Add authentication in moment GET api

--- backend/moment/writing/views.py
+++ backend/moment/writing/views.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from rest_framework import permissions
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
@@ -9,17 +10,20 @@ from .serializers import MomentPairQuerySerializer, MomentPairSerializer
 
 # Create your views here.
 class MomentView(GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = MomentPairSerializer
 
     def get(self, request):
         params = MomentPairQuerySerializer(data=request.query_params)
         params.is_valid(raise_exception=True)
-        
+        user = request.user
+
         start_date = datetime.fromtimestamp(params.validated_data["start"])
         end_date = datetime.fromtimestamp(params.validated_data["end"])
 
         moments = MomentPair.objects.filter(
-            moment_created_at__range=(start_date, end_date)
+            moment_created_at__range=(start_date, end_date),
+            user=user,
         )
         serializer = self.get_serializer(data=moments, many=True)
         serializer.is_valid()
