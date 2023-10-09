6d6cfd562fc247cbf322eda17ed84044dfa162e8
박정헌 <impri@naver.om>
2023-10-09 11:33:14 +0900

    feat: Added story and moment models

---
+++ backend/moment/writing/constants.py
@@ -0,0 +1,12 @@
+EMOTIONS_CHOICES = [
+    ("excited1", "excited1"),
+    ("excited2", "excited2"),
+    ("happy1", "happy1"),
+    ("happy2", "happy2"),
+    ("normal1", "normal1"),
+    ("normal2", "normal2"),
+    ("sad1", "sad1"),
+    ("sad2", "sad2"),
+    ("angry1", "angry1"),
+    ("angry2", "angry2"),
+]
---
+++ backend/moment/writing/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from moment.user import User
+from constants import EMOTIONS_CHOICES
+
+# Create your models here.
+class Story(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_created=True)
+    emotion = models.CharField(
+        max_length=100, choices=EMOTIONS_CHOICES, default="normal1"
+    )
+    score = models.IntegerField(default=3)
+    is_point_completed = models.BooleanField(default=False)
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=1000)
+
+
+class MomentPair(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    moment = models.CharField(max_length=1000)
+    reply = models.CharField(max_length=1000, blank=True)
+    moment_created_at = models.DateTimeField()
+    reply_created_at = models.DateTimeField()
+    story_id = models.ForeignKey(Story, null=True)
