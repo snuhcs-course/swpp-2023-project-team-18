7be6786ca63b8dd586d7dfdec001e22b37be8179
impri2 <impri@naver.com>
2023-10-10 10:58:46 +0900

    feat: Add gpt call logic to get reply

--- backend/moment/writing/views.py
+++ backend/moment/writing/views.py
@@ -11,6 +11,7 @@ from .serializers import (
     MomentPairCreateSerializer,
 )
 from user.models import User
+from .utils import call_gpt, GPTError
 
 
 # Create your views here.
@@ -33,7 +34,8 @@ class MomentView(GenericAPIView):
         serializer = self.get_serializer(moment_pairs, many=True)
 
         return Response(
-            {"moments": serializer.data},
+            data={"moments": serializer.data},
+            status=200,
         )
 
     def post(self, request):
@@ -43,11 +45,18 @@ class MomentView(GenericAPIView):
 
         # TODO: openai api 써서 reply 받아오기
         #   만약 실패하면 그냥 빈 문자열로 저장하나? 아니면 moment 저장 자체를 취소하나?
+        try:
+            reply = call_gpt(body.data["moment"], timeout=5)  # TODO: 프롬프팅 처리 하기
+        except GPTError:
+            return Response(
+                data={"error": "GPT3 API call failed"},
+                status=500,
+            )
 
         moment_pair = MomentPair.objects.create(
             user=user,
             moment=body.data["moment"],
-            reply="",  # TODO: 나중에 붙이기
+            reply=reply,
             story=None,
             moment_created_at=datetime.now(),
             reply_created_at=datetime.now(),
@@ -57,5 +66,6 @@ class MomentView(GenericAPIView):
         serializer = self.get_serializer(moment_pair)
 
         return Response(
-            {"moment": serializer.data},
+            data={"moment": serializer.data},
+            status=201,
         )
