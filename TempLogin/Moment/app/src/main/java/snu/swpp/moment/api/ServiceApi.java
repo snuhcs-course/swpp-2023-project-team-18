ad36aacd106e551c993d1e514f224db233cb868c
Sungmin <sungsung718@gmail.com>
2023-10-14 15:57:41 +0900

    fix: Remove unused directory TempLogin

--- TempLogin/Moment/app/src/main/java/snu/swpp/moment/api/ServiceApi.java
+++
@@ -1,9 +0,0 @@
-package snu.swpp.moment.api;
-import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.POST;
-public interface ServiceApi {
-    @POST("/api/user/login")
-    Call<LoginResponse> userLogin(@Body LoginRequest data);
-
-}
