5adffc2237a0141afdb7be7d85a1658384e8bcbd
Sungmin <sungsung718@gmail.com>
2023-10-21 17:53:40 +0900

    fix: Remove error message while typing username or password in login page

--- frontend/Moment/app/src/main/java/snu/swpp/moment/ui/login/LoginViewModel.java
+++ frontend/Moment/app/src/main/java/snu/swpp/moment/ui/login/LoginViewModel.java
@@ -56,29 +56,6 @@ public class LoginViewModel extends ViewModel {
     }
 
     public void loginDataChanged(String username, String password) {
-        if (!isUserNameValid(username)) {
-            loginFormState.setValue(new LoginFormState(R.string.invalid_username, null));
-        } else if (!isPasswordValid(password)) {
-            loginFormState.setValue(new LoginFormState(null, R.string.invalid_password));
-        } else {
-            loginFormState.setValue(new LoginFormState(true));
-        }
-    }
-
-    // A placeholder username validation check
-    private boolean isUserNameValid(String username) {
-        if (username == null) {
-            return false;
-        }
-        if (username.contains("@")) {
-            return Patterns.EMAIL_ADDRESS.matcher(username).matches();
-        } else {
-            return !username.trim().isEmpty();
-        }
-    }
-
-    // A placeholder password validation check
-    private boolean isPasswordValid(String password) {
-        return password != null && password.trim().length() > 5;
+        loginFormState.setValue(new LoginFormState(true));
     }
 }
