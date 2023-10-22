ec5fe663264d11a6b6cbc7a2a98d107952ed5070
sungsung718 <sungsung718@gmail.com>
2023-10-23 08:26:07 +0900

    feat: Add string resources for write view

--- frontend/Moment/app/src/main/java/snu/swpp/moment/ui/main_writeview/ListViewAdapter.java
+++ frontend/Moment/app/src/main/java/snu/swpp/moment/ui/main_writeview/ListViewAdapter.java
@@ -51,8 +51,10 @@ public class ListViewAdapter extends BaseAdapter {
         ListViewItem item = items.get(position);
         userInput.setText(item.getUserInput());
         inputTime.setText(item.getInputTime());
-        serverResponse.setText(item.getServerResponse().isEmpty() ? "Waiting for server response..."
-            : item.getServerResponse());
+        serverResponse.setText(
+            item.getServerResponse().isEmpty() ? convertView.getResources()
+                .getString(R.string.moment_reply_loading)
+                : item.getServerResponse());
         //serverResponse.setText("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
         return convertView;
     }
