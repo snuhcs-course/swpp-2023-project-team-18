16817741bd2b21fc4f230950eb55e6e1b5bf2fc9
Ayoung Lee <aylee2008@naver.com>
2023-10-26 19:46:37 +0900

    fix: Change some constants

--- backend/moment/writing/constants.py
+++ backend/moment/writing/constants.py
@@ -2,9 +2,9 @@ import os
 from enum import Enum, EnumMeta
 
 
-MOMENT_MAX_LENGTH = 1000
-STORY_MAX_LENGTH = 10000
-STORY_TITLE_MAX_LENGTH = 100
+MOMENT_MAX_LENGTH = 100
+STORY_MAX_LENGTH = 1000
+STORY_TITLE_MAX_LENGTH = 20
 
 
 class MetaEnum(EnumMeta):
@@ -31,6 +31,7 @@ class Emotions(str, BaseEnum):
     SAD2 = "sad2"
     ANGRY1 = "angry1"
     ANGRY2 = "angry2"
+    INVALID = "invalid"
 
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
