750043ff0aefba9a3e9f0c88f158a8ab323e9b14
footprinthere <tai15515@kakao.com>
2023-10-27 15:19:56 +0900

    feat: Implement StoryUiState

---
+++ frontend/Moment/app/src/main/java/snu/swpp/moment/ui/main_writeview/DaySlide/StoryUiState.kt
@@ -0,0 +1,12 @@
+package snu.swpp.moment.ui.main_writeview.DaySlide
+
+class StoryUiState(
+    val error: Int,
+    val storyTitle: String,
+    val storyContent: String,
+    val emotion: Int,
+    val tags: List<String>,
+    val score: Int,
+) {
+
+}
