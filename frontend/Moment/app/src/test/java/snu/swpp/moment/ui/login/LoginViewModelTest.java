186c90035ebb2b1813d5bacaf654a58ba622a699
Ayoung Lee <aylee2008@naver.com>
2023-11-01 22:02:41 +0900

    feat: Add initial structure of login test

---
+++ frontend/Moment/app/src/test/java/snu/swpp/moment/ui/login/LoginViewModelTest.java
@@ -0,0 +1,18 @@
+package snu.swpp.moment.ui.login;
+
+import junit.framework.TestCase;
+
+public class LoginViewModelTest extends TestCase {
+
+    public void testGetLoginFormState() {
+    }
+
+    public void testGetLoginResult() {
+    }
+
+    public void testLogin() {
+    }
+
+    public void testLoginDataChanged() {
+    }
+}
