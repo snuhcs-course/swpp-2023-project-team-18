a1df5f6b3c0647ad7747f0fc687ee156fa817e90
Ayoung Lee <aylee2008@naver.com>
2023-11-09 20:57:06 +0900

    feat: Add initial structure of nudge model

--- backend/moment/writing/models.py
+++ backend/moment/writing/models.py
@@ -46,8 +46,11 @@ class MomentPair(models.Model):
 
 class Nudge(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    story = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField()
+    summarized_story = models.CharField(max_length=1000)
     content = models.CharField(max_length=1000)
-    is_completed = models.BooleanField(default=False)
+    should_be_displayed = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_created=False)
