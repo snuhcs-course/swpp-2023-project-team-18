438752c926f540b1e03969ac0827fadb6ddb2999
impri2 <impri@naver.com>
2023-11-10 18:00:57 +0900

    feat: Stat page design fix

--- frontend/Moment/app/src/main/java/snu/swpp/moment/ui/main_statview/StatViewFragment.kt
+++ frontend/Moment/app/src/main/java/snu/swpp/moment/ui/main_statview/StatViewFragment.kt
@@ -1,19 +1,19 @@
 package snu.swpp.moment.ui.main_statview
 
 import android.content.Context
-import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.JavascriptInterface
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
@@ -118,23 +118,32 @@ class StatViewFragment : Fragment() {
             entries.add(Entry((-day).toFloat(), score.toFloat()))
         }
         val dataset = LineDataSet(entries,null)
+        val assetManager = requireContext().assets
+        dataset.circleColors = listOf(requireContext().getColor(R.color.black))
+        dataset.color = requireContext().getColor(R.color.black)
+        dataset.setDrawCircleHole(false)
+        dataset.setDrawValues(false)
+
         lineChart.description = null
         lineChart.legend.isEnabled = false
+        lineChart.isScaleYEnabled = false
 
         lineChart.xAxis.valueFormatter = DateAxisValueFormat(today)
         lineChart.xAxis.isGranularityEnabled = true
         lineChart.xAxis.granularity = 1.0F
         lineChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
-        lineChart.axisLeft.axisMinimum = 1.0F
+        lineChart.axisLeft.axisMinimum = 0.0F
         lineChart.axisLeft.axisMaximum = 5.0F
-        lineChart.axisRight.axisMinimum = 1.0F
+        lineChart.axisRight.axisMinimum = 0.0F
         lineChart.axisRight.axisMaximum = 5.0F
-        lineChart.axisLeft.setLabelCount(5,true);
-        lineChart.axisRight.setLabelCount(5,true);
+        lineChart.axisLeft.setLabelCount(6,true);
+        lineChart.axisLeft.typeface = ResourcesCompat.getFont(requireContext(),R.font.maruburi_light)
+        lineChart.axisRight.setLabelCount(6,true);
         lineChart.axisRight.isEnabled = false
         dataset.setDrawHorizontalHighlightIndicator(false);
         dataset.setDrawVerticalHighlightIndicator(false);
         lineChart.xAxis.setLabelCount(entries.size,false)
+        lineChart.xAxis.typeface = ResourcesCompat.getFont(requireContext(),R.font.maruburi_light)
 
 
         lineChart.axisLeft.valueFormatter = (object : ValueFormatter() {
@@ -204,28 +213,76 @@ class StatViewFragment : Fragment() {
         val pieChart = binding.statPieChart
         var pie : MutableList<PieEntry> = mutableListOf()
         var colors:MutableList<Int> = mutableListOf()
+        val emotions = sampleEmotions()
         for(emotion in emotions){
             pie.add(PieEntry(emotion.value.toFloat(),emotion.key))
             colors.add(getEmotionColor(emotion.key))
 
         }
-        val pieDataset = PieDataSet(pie,"감정")
+        val pieDataset = PieDataSet(pie,"")
         pieDataset.colors = colors
+        pieChart.legend.verticalAlignment = Legend.LegendVerticalAlignment.CENTER
+        pieChart.legend.horizontalAlignment = Legend.LegendHorizontalAlignment.RIGHT
+        pieChart.legend.orientation = Legend.LegendOrientation.VERTICAL
+        pieChart.legend.setDrawInside(false);
+        pieChart.legend.textSize = 12f
+        pieChart.legend.typeface = ResourcesCompat.getFont(requireContext(),R.font.maruburi_light)
+        pieChart.legend.form = Legend.LegendForm.CIRCLE
+        pieChart.description.isEnabled = false
+        pieChart.setDrawEntryLabels(false)
+        pieDataset.valueFormatter = object: ValueFormatter() {
+            override fun getFormattedValue(value: Float): String {
+                return "${value / emotions.size * 100} %"
+            }
+        }
+        pieDataset.valueTextColor = requireContext().getColor(R.color.white)
+        pieDataset.valueTextSize = 12f
+        pieDataset.valueTypeface = ResourcesCompat.getFont(requireContext(),R.font.maruburi_bold)
+
+
+
         pieChart.data = PieData(pieDataset)
         pieChart.invalidate()
 
     }
+    fun sampleEmotions():Map<String,Int>{//FIXME: test 후 지우기
+        val m:MutableMap<String,Int> = mutableMapOf()
+        m["기쁨"]=1
+        m["설렘"]=2
+        m["행복"]=1
+        m["신남"]=1
+        m["평범"]=1
+        m["모름"]=1
+        m["짜증"]=1
+        m["화남"]=1
+    //    m["슬픔"]=1
+     //   m["우울"]=1
+        return m
+
+    }
     fun getEmotionColor(emotion:String):Int{
-        if(emotion.equals("설렘")||emotion.equals("신남"))
-            return requireContext().getColor(R.color.stat_emotion_12)
-        else if(emotion.equals("기쁨")||emotion.equals("행복"))
-            return requireContext().getColor(R.color.stat_emotion_34)
-        else if(emotion.equals("평범")||emotion.equals("모름"))
-            return requireContext().getColor(R.color.stat_emotion_56)
-        else if(emotion.equals("슬픔")||emotion.equals("우울"))
-            return requireContext().getColor(R.color.stat_emotion_78)
+        if(emotion.equals("설렘"))
+            return requireContext().getColor(R.color.stat_emotion_1)
+        else if(emotion.equals("신남"))
+            return requireContext().getColor(R.color.stat_emotion_2)
+        else if(emotion.equals("기쁨"))
+            return requireContext().getColor(R.color.stat_emotion_3)
+        else if(emotion.equals("행복"))
+            return requireContext().getColor(R.color.stat_emotion_4)
+        else if(emotion.equals("평범"))
+            return requireContext().getColor(R.color.stat_emotion_5)
+        else if(emotion.equals("모름"))
+            return requireContext().getColor(R.color.stat_emotion_6)
+        else if(emotion.equals("슬픔"))
+            return requireContext().getColor(R.color.stat_emotion_7)
+        else if(emotion.equals("우울"))
+            return requireContext().getColor(R.color.stat_emotion_8)
+        else if(emotion.equals("화남"))
+            return requireContext().getColor(R.color.stat_emotion_9)
         else
-            return requireContext().getColor(R.color.stat_emotion_910)
+            return requireContext().getColor(R.color.stat_emotion_10)
+
+
     }
     override fun onDestroyView() {
         super.onDestroyView()
