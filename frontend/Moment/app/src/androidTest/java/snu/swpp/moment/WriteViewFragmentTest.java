5073ff96dc4e588d1d7f5f89399d65e3212afda4
Sungmin <sungsung718@gmail.com>
2023-11-17 17:04:15 +0900

    fix: Fix WriteViewFragmentTest to work by adding custom ViewActions

--- frontend/Moment/app/src/androidTest/java/snu/swpp/moment/WriteViewFragmentTest.java
+++ frontend/Moment/app/src/androidTest/java/snu/swpp/moment/WriteViewFragmentTest.java
@@ -1,30 +1,21 @@
 package snu.swpp.moment;
 
-import static androidx.core.content.ContextCompat.startActivity;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.contrib.NavigationViewActions.navigateTo;
-import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.not;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.doReturn;
 
-import android.content.Intent;
-import android.util.Log;
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.IdlingResource;
-import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.espresso.matcher.ViewMatchers.Visibility;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -36,38 +27,39 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.Spy;
-import snu.swpp.moment.data.callback.AuthenticationCallBack;
-import snu.swpp.moment.data.callback.TokenCallBack;
-import snu.swpp.moment.data.model.LoggedInUserModel;
 import snu.swpp.moment.data.repository.AuthenticationRepository;
 import snu.swpp.moment.ui.login.LoginActivity;
-import snu.swpp.moment.ui.main_writeview.viewmodel.TodayViewModel;
+import snu.swpp.moment.utils.CustomViewAction;
 import snu.swpp.moment.utils.TimeConverter;
+import snu.swpp.moment.utils.ViewPager2Actions;
 
 @RunWith(AndroidJUnit4.class)
 public class WriteViewFragmentTest {
+
     private AuthenticationRepository authenticationRepository;
     private IdlingResource idlingResource;
 
     @Rule
-    public ActivityScenarioRule<LoginActivity> scenarioRule = new ActivityScenarioRule<>(LoginActivity.class);
+    public ActivityScenarioRule<LoginActivity> scenarioRule = new ActivityScenarioRule<>(
+        LoginActivity.class);
+
+    @Rule
+    public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
 
     @Before
     public void setUp() throws GeneralSecurityException, IOException {
-        authenticationRepository = AuthenticationRepository.getInstance(ApplicationProvider.getApplicationContext());
+        authenticationRepository = AuthenticationRepository.getInstance(
+            ApplicationProvider.getApplicationContext());
         idlingResource = authenticationRepository.getIdlingResource();
-        IdlingRegistry.getInstance().register();
+        IdlingRegistry.getInstance().register(idlingResource);
 
         onView(withId(R.id.username)).perform(click(), replaceText("test"));
         onView(withId(R.id.password)).perform(click(), replaceText("123456"));
-        onView(withId(R.id.login_button)).perform(click());
-        scenarioRule.getScenario().onActivity(activity -> {
-            activity.startActivity(new Intent(activity, MainActivity.class));
-        });
+
+        // click()을 사용하면 알 수 없는 이유로 LoginActivy에 있는 login button의 listener가 불리지 않음.
+        onView(withId(R.id.login_button))
+            .check(matches(withText(R.string.action_log_in)))
+            .perform(CustomViewAction.forceClick());
     }
 
     @After
@@ -77,11 +69,7 @@ public class WriteViewFragmentTest {
 
     @Test
     public void whenTodayViewFragmentLaunched_ToolBarTitleDisplaysToday() {
-        LocalDate today = TimeConverter.getToday();
-        String formattedDate = TimeConverter.formatLocalDate(today, "yyyy. MM. dd.");
-        String dayOfWeek = today.getDayOfWeek().getDisplayName(
-            java.time.format.TextStyle.SHORT, java.util.Locale.KOREAN);
-        String title = formattedDate + ' ' + dayOfWeek;
+        String title = getTitle(0);
         onView(withId(R.id.text_title)).check(matches(withText(title)));
     }
 
@@ -91,16 +79,27 @@ public class WriteViewFragmentTest {
     }
 
     @Test
-    public void whenTodayViewFragmentLaunched_MomentEditTextIsInvisible() {
-        onView(allOf(withId(R.id.momentEditText))).check(matches(not(isDisplayed())));
+    public void whenAddButtonClicked_MomentEditTextIsClickable() {
+        // 미리 만들어지는 fragment마다 addButton이 만들어지기 때문에 그 중에 현재 보이는 것만을 클릭
+        onView(allOf(withId(R.id.addButton), withEffectiveVisibility(Visibility.VISIBLE)))
+            .perform(click());
+        onView(allOf(withId(R.id.momentEditText), withEffectiveVisibility(Visibility.VISIBLE)))
+            .check(matches(isClickable()));
     }
 
     @Test
-    public void whenAddButtonClicked_EditTextIsDisplayed() {
-        // TODO
-        onView(allOf(withId(R.id.addButton)))
-            .perform(click())
-            .check(matches(not(isDisplayed())));
-        onView(anyOf(withId(R.id.momentEditText))).check(matches(isDisplayed()));
+    public void whenPageScrolledToLeft_DailyViewFragmentIsLaunched() {
+        String title = getTitle(1);
+        onView(withId(R.id.viewpager)).perform(ViewPager2Actions.scrollLeft());
+        onView(withId(R.id.text_title)).check(matches(withText(title)));
+        onView(withId(R.id.backToTodayButton)).check(matches(isDisplayed()));
+    }
+
+    private String getTitle(int offset) {
+        LocalDate day = TimeConverter.getToday().minusDays(offset);
+        String formattedDate = TimeConverter.formatLocalDate(day, "yyyy. MM. dd.");
+        String dayOfWeek = day.getDayOfWeek().getDisplayName(
+            java.time.format.TextStyle.SHORT, java.util.Locale.KOREAN);
+        return formattedDate + ' ' + dayOfWeek;
     }
 }
---
+++ frontend/Moment/app/src/androidTest/java/snu/swpp/moment/utils/CustomViewAction.java
@@ -0,0 +1,33 @@
+package snu.swpp.moment.utils;
+
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
+import static org.hamcrest.Matchers.allOf;
+
+import android.view.View;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import org.hamcrest.Matcher;
+
+public class CustomViewAction {
+    public static ViewAction forceClick() {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return allOf(isClickable(), isEnabled(), isDisplayed());
+            }
+
+            @Override
+            public String getDescription() {
+                return "force click";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                view.performClick(); // perform click without checking view coordinates.
+                uiController.loopMainThreadUntilIdle();
+            }
+        };
+    }
+}
